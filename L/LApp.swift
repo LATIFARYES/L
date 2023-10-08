cdc2749fedcebc27886a617b473780a5b2d1f94b
LATIFA <latifaryes@gmail.com>
2023-10-08 14:42:16 +0300

    Initial Commit

---
+++ L/LApp.swift
@@ -0,0 +1,17 @@
+//
+//  LApp.swift
+//  L
+//
+//  Created by LATIFA on 08/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct LApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
