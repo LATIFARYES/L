604115fa3701f7929b95511e1354cb0b5c541ddb
LATIFARYES <latifaryes@gmail.com>
2023-10-08 23:08:43 +0300

    i delet the first line

--- L/ContentView.swift
+++ L/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+   
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+       
+        HStack {
+            
+           
+            
+            Text(" First ")
+            Text(" Middle ")
+                .underline()
+            Text(" Last ")
+            
         }
         .padding()
+        .border(Color.yellow)
+    
     }
 }
 
